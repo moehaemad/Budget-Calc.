903f577312fd97212ecd2ac2e7fe8890e8114fdf
Muhammad <muhammad.ali.72.27@gmail.com>
2018-06-19 18:26:45 -0700

    frames destroyed at start of function (fixed)

--- code/main.py
+++ code/main.py
@@ -19,7 +19,9 @@ def close (master):
     
 def create_frames(master):
     statframe = tk.Frame(master)
-    return [statframe]
+    frames = list()
+    frames.append(statframe)
+    return frames
 
 def destroy_frames(frames):
     for frame in frames:
@@ -30,11 +32,10 @@ def load_dataset(filename):
         return pd.read_csv(f, parse_dates = True)
     
 def statistics (frames,X):
-    destroy_frames(frames)
-    statframe = frames[0]
-    pdb.set_trace()
-    tk.Label(statframe, text="The latest " + str(NUMLATEST) +"entries" 
-             +"of your statement").pack(side=tk.LEFT)
+    statframe = frames.pop(0)
+    if (len(frames)>=1):
+        destroy_frames(frames)
+    tk.Label(statframe, text="The latest").pack()
     fiveFreq = tk.Listbox(statframe)
     for i in range(0,NUMLATEST):
         fiveFreq.insert(i,X[i,1])
@@ -46,13 +47,14 @@ def line_plot(frames, X, master):
     f = fig.add_subplot(111)
     X[pd.isnull(X[:,3]),3]=0
     X[pd.isnull(X[:,2]),2]=0
+    pdb.set_trace()
     spending = X[:,2]*-1 + X[:,3]
 #    pdb.set_trace()
     #TODO: find way to display years as x labels for a number of transactions
     #that isn't clustered
         #TODO: Create datetime objects for the dates
     #TODO: Find way to delete canvas to make the GUI more dynamic
-    f.plot (X[:,0],spending, scalex=True)
+    f.plot (X[:,0], scalex=True)
     canvas = FigureCanvasTkAgg (fig, master)
     canvas.show()
     canvas.get_tk_widget().pack(side=tk.TOP, fill = tk.BOTH, expand=True)
