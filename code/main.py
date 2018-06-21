12f3212acbcefa69349e7bb3f1e8381d28185fb0
Muhammad <muhammad.ali.72.27@gmail.com>
2018-06-20 19:56:12 -0700

    cleaned up main code and found a way to sort names alph.

--- code/main.py
+++ code/main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 import numpy as np
 import pdb
-import monthly
+from monthly import Monthly
 from dates import Dates as dt
 import tkinter as tk
 
@@ -15,7 +15,7 @@ import tkinter as tk
 # -Find way to delete canvas to make the GUI more dynamic (i.e. either
     #remove frames or canvas and use one)
 NUMLATEST = 10
-def close (master):
+def close(master):
     master.destroy()
     
 def create_frames(master):
@@ -25,35 +25,35 @@ def create_frames(master):
     return frames
 
 def destroy_frames(frames):
-    if (len(frames)==0):
+    if (len(frames) == 0):
         return 0
     for frame in frames:
         frame.destroy()
 
 def load_dataset(filename):
-    with open (os.path.join ('..','data',filename), 'rb') as f:
+    with open(os.path.join ('..','data', filename), 'rb') as f:
         return pd.read_csv(f, parse_dates = True)
     
-def statistics (frames,X):
+def statistics(frames, X):
     statframe = frames.pop(0)
-    if (len(frames)>=1):
+    if (len(frames) >= 1):
         destroy_frames(frames)
     tk.Label(statframe, text="The latest").pack()
     fiveFreq = tk.Listbox(statframe)
-    for i in range(0,NUMLATEST):
-        fiveFreq.insert(i,X[i,1])
+    for i in range(0, NUMLATEST):
+        fiveFreq.insert(i, X[i,1])
     fiveFreq.pack(side=tk.RIGHT)
     statframe.pack(expand=tk.YES)
     
 def line_plot(frames, X, master):
     destroy_frames(frames)
-    fig = Figure (figsize=(5,5),dpi=100)
+    fig = Figure (figsize=(5, 5), dpi=100)
     f = fig.add_subplot(111)
-    X[pd.isnull(X[:,3]),3]=0
-    X[pd.isnull(X[:,2]),2]=0
-    spending = X[:,2]*-1 + X[:,3]
-    years = [i[0:4] for i in X[:,0]]
-    f.plot (years, spending,scalex=True)
+    X[pd.isnull(X[:, 3]),3]=0
+    X[pd.isnull(X[:, 2]),2]=0
+    spending = X[:, 2]*-1 + X[:, 3]
+    years = [i[0:4] for i in X[:, 0]]
+    f.plot(years, spending, scalex=True)
     canvas = FigureCanvasTkAgg (fig, master)
     canvas.show()
     canvas.get_tk_widget().pack(side=tk.TOP, fill = tk.BOTH, expand=True)
@@ -62,17 +62,17 @@ def line_plot(frames, X, master):
 
 if __name__ == '__main__':
     read = load_dataset("visa.csv")
-    X = read.values
+    X = read.as_matrix()
 #    pdb.set_trace()
-    monthly.find_Monthly_Expenditure(X)
+    monthly = Monthly()
+    monthly.find_Monthly_Expenditure(X=X)
     root = tk.Tk()
     mainmenu = tk.Menu(root)
     frames = create_frames(root)
     mainmenu.add_command(label="stop", command = lambda: close(master=root))
-    statmenu = tk.Menu(mainmenu,tearoff=0)
+    statmenu = tk.Menu(mainmenu, tearoff=0)
     statmenu.add_command(label= str(NUMLATEST)+" latest transactions", 
-                         command = 
-                         lambda: statistics(frames,X))
+                         command = lambda: statistics(frames,X))
     statmenu.add_command(label="linear graph trans.", command = lambda: 
         line_plot(frames, X, root))
     mainmenu.add_cascade(label="stats", menu=statmenu)
--- code/monthly.py
+++ code/monthly.py
@@ -1,21 +1,23 @@
 import numpy as np
 
-def create_dictionary (X):
-    return 0
-
-
-#TODO: sort the array of X[:,[0,1]] s.t. name and date are sorted together
-    #respectively. Sort on X[:,1](names) alphabetically
-def find_Monthly_Expenditure (self, X,*args):
-#    frequencies = dict(zip(names,counts))
-#    print (frames) #don't think I need frames but not sure right now
+class Monthly:
+    def create_dictionary (X):
+        return 0
     
-    create_dictionary (X[:,[0,1]])
-
-def manual_entry(self, names):
-    for i in range(len(names)):
-        answer = input ("Is " + names[i] + " a monthly ependiture")
-        if (answer == "y"):
-            self.monthlies[i] = names[i]
-
-        
+    
+    #TODO: sort the array of X[:,[0,1]] s.t. name and date are sorted together
+        #respectively. Sort on X[:,1](names) alphabetically
+    def find_Monthly_Expenditure (self, X,*args):
+    #    frequencies = dict(zip(names,counts))
+    #    print (frames) #don't think I need frames but not sure right now
+        _sortedplaces = X[np.argsort(X[:,1]), :]
+        import pdb; pdb.set_trace()
+        create_dictionary (X[:,[0,1]])
+    
+    def manual_entry(self, names):
+        for i in range(len(names)):
+            answer = input ("Is " + names[i] + " a monthly ependiture")
+            if (answer == "y"):
+                self.monthlies[i] = names[i]
+    
+            
